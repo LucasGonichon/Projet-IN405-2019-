87700f6a6477636ab7cfc736289eb3323d598de7
Lucas Gonichon <lucasgonichon@gmail.com>
2019-04-22 17:53:49 +0200

    lecture v.1.0

--- lecture.c
+++ lecture.c
@@ -4,19 +4,54 @@
 //nbJoueurs ;Cmax;Kmax;nbTours
 //2  ;  1  0  0  ;  1  0  0  ;  2  0 
 
-typedef struct{
-	map_t type;
-	coord_t taille;
-	int nbjoueurs;
-	int cmax;
-	int kmax;
-	int nbtour;
-}info;
+#include <navalmap.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-info lire_fichier(char* nomf){
-	info inf;
+typedef struct {
+	navalmap_t navalmap;
+	int Cmax, Kmax, nbTours;
+} game_info;
+
+game_info lire_fichier(char* nomf){
 	
-	
-	
-	return inf;
+	FILE * f = fopen (nomf, "r");
+
+	game_info res;
+
+	if (f != NULL) {
+
+		navalmap_t * navalmap;
+
+		char * type_carte;
+		fscanf (f, "%s", type_carte);
+
+		coord_t size;
+		fscanf (f, "%d %d", &size.x, &size.y);
+
+		int nbShips;
+		fscanf (f, "%d", &nbShips);
+
+		if (strcmp (type_carte, "rectangle") == 0)
+			navalmap = init_navalmap (MAP_RECT, size, nbShips);
+
+		else {
+			printf ("type de carte invalide");
+			exit (1);
+		}
+
+		fscanf (f, "%d %d %d", &res.Cmax, &res.Kmax, &res.nbTours);
+
+		res.navalmap = *navalmap;
+
+		fclose (f);
+	}
+
+	else {
+		printf ("échec du chargement du niveau : %s", nomf);
+		exit (1);
+	}
+
+	return res;
 }
